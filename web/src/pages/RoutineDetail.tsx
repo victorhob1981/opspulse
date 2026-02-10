3237eb9c430c1a6fe0153a217811c89d9c396edf
victorhob1981 <victorhob@ufrrj.br>
2026-02-10 16:05:40 -0300

    refactor: remove React import as it is not needed

--- web/src/pages/RoutineDetail.tsx
+++ web/src/pages/RoutineDetail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { apiFetch } from "../lib/api";
 import { dueLabel, formatDateTime, formatDuration, relativeTime } from "../lib/format";
